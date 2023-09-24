84eb120be5c3492df967586e14f001499117e2e6
Youngwook Kim <youngwook.kim@gmail.com>
2023-09-24 17:32:17 +0900

    style: replace double quotes with single quotes in various files (#16)

--- api/app/clients/tools/WebQA.ts
+++ api/app/clients/tools/WebQA.ts
@@ -1,5 +1,5 @@
 import { StructuredTool, ToolParams } from 'langchain/tools';
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { loadQARefineChain, RetrievalQAChain } from 'langchain/chains';
 import { Document } from 'langchain/document';
 import { BaseDocumentLoader } from 'langchain/document_loaders/base';
@@ -11,220 +11,216 @@ import { LaunchOptions, Page, Browser, Response, Frame, Locator } from 'playwrig
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 export type PlaywrightGotoOptions = {
-    referer?: string;
-    timeout?: number;
-    waitUntil?: "load" | "domcontentloaded" | "networkidle" | "commit";
+  referer?: string;
+  timeout?: number;
+  waitUntil?: 'load' | 'domcontentloaded' | 'networkidle' | 'commit';
 };
 
 export type PlaywrightEvaluate = (
-    page: Page,
-    browser: Browser,
-    response: Response | null
+  page: Page,
+  browser: Browser,
+  response: Response | null,
 ) => Promise<string>;
 
 export type PlaywrightWebBaseLoaderOptions = {
-    launchOptions?: LaunchOptions;
-    gotoOptions?: PlaywrightGotoOptions;
-    evaluate?: PlaywrightEvaluate;
+  launchOptions?: LaunchOptions;
+  gotoOptions?: PlaywrightGotoOptions;
+  evaluate?: PlaywrightEvaluate;
 };
 
-export class PlaywrightWebBaseLoader
-    extends BaseDocumentLoader
-    implements DocumentLoader {
-    options: PlaywrightWebBaseLoaderOptions | undefined;
+export class PlaywrightWebBaseLoader extends BaseDocumentLoader implements DocumentLoader {
+  options: PlaywrightWebBaseLoaderOptions | undefined;
 
-    constructor(
-        public webPath: string,
-        options?: PlaywrightWebBaseLoaderOptions
-    ) {
-        super();
-        this.options = options ?? undefined;
+  constructor(public webPath: string, options?: PlaywrightWebBaseLoaderOptions) {
+    super();
+    this.options = options ?? undefined;
+  }
+
+  static async _scrape(url: string, options?: PlaywrightWebBaseLoaderOptions): Promise<string> {
+    const { chromium } = await PlaywrightWebBaseLoader.imports();
+
+    const browser = await chromium.launch({
+      headless: true,
+      ...options?.launchOptions,
+    });
+    const page = await browser.newPage();
+
+    const response = await page.goto(url, {
+      timeout: 180000,
+      waitUntil: 'domcontentloaded',
+      ...options?.gotoOptions,
+    });
+    const bodyHTML = options?.evaluate
+      ? await options?.evaluate(page, browser, response)
+      : await page.content();
+
+    await browser.close();
+
+    return bodyHTML;
+  }
+
+  async scrape(): Promise<string> {
+    return PlaywrightWebBaseLoader._scrape(this.webPath, this.options);
+  }
+
+  async load(): Promise<Document[]> {
+    const text = await this.scrape();
+
+    const metadata = { source: this.webPath };
+    return [new Document({ pageContent: text, metadata })];
+  }
+
+  static async imports(): Promise<{
+    chromium: typeof import('playwright').chromium;
+  }> {
+    try {
+      const { chromium } = await import('playwright');
+
+      return { chromium };
+    } catch (e) {
+      console.error(e);
+      throw new Error('Please install playwright as a dependency with, e.g. `yarn add playwright`');
     }
-
-    static async _scrape(
-        url: string,
-        options?: PlaywrightWebBaseLoaderOptions
-    ): Promise<string> {
-        const { chromium } = await PlaywrightWebBaseLoader.imports();
-
-        const browser = await chromium.launch({
-            headless: true,
-            ...options?.launchOptions,
-        });
-        const page = await browser.newPage();
-
-        const response = await page.goto(url, {
-            timeout: 180000,
-            waitUntil: "domcontentloaded",
-            ...options?.gotoOptions,
-        });
-        const bodyHTML = options?.evaluate
-            ? await options?.evaluate(page, browser, response)
-            : await page.content();
-
-        await browser.close();
-
-        return bodyHTML;
-    }
-
-    async scrape(): Promise<string> {
-        return PlaywrightWebBaseLoader._scrape(this.webPath, this.options);
-    }
-
-    async load(): Promise<Document[]> {
-        const text = await this.scrape();
-
-        const metadata = { source: this.webPath };
-        return [new Document({ pageContent: text, metadata })];
-    }
-
-    static async imports(): Promise<{
-        chromium: typeof import("playwright").chromium;
-    }> {
-        try {
-            const { chromium } = await import("playwright");
-
-            return { chromium };
-        } catch (e) {
-            console.error(e);
-            throw new Error(
-                "Please install playwright as a dependency with, e.g. `yarn add playwright`"
-            );
-        }
-    }
+  }
 }
 
 const getMaxTokens = (modelName: string): number => {
-    if (modelName.startsWith('gpt-3.5-turbo-16k')) {
-        return 16384;
-    }
-    if (modelName.startsWith('gpt-4-32k')) {
-        return 32768;
-    }
-    if (modelName.startsWith('gpt-4')) {
-        return 8192;
-    }
-    return 4096;
+  if (modelName.startsWith('gpt-3.5-turbo-16k')) {
+    return 16384;
+  }
+  if (modelName.startsWith('gpt-4-32k')) {
+    return 32768;
+  }
+  if (modelName.startsWith('gpt-4')) {
+    return 8192;
+  }
+  return 4096;
 };
 
 // If it can be created as a separate Document, would it be better?
 const evaluateFrameContent = async (page: Page | Frame): Promise<string> => {
-    const scripts: Locator = page.locator('script');
-    await scripts.evaluateAll(scripts => scripts.forEach(script => script.remove()));
+  const scripts: Locator = page.locator('script');
+  await scripts.evaluateAll((scripts) => scripts.forEach((script) => script.remove()));
 
-    const body = page.locator('body');
-    const bodyText = await body.innerText();
-    const title = await page.title();
+  const body = page.locator('body');
+  const bodyText = await body.innerText();
+  const title = await page.title();
 
-    if (title) {
-        return `# ${title}\n\n${bodyText}`;
-    }
+  if (title) {
+    return `# ${title}\n\n${bodyText}`;
+  }
 
-    return bodyText;
+  return bodyText;
 };
 
 interface WebQASchema {
-    question: string;
-    url: string;
+  question: string;
+  url: string;
 }
 
 interface WebQAArgs extends ToolParams {
-    embeddings: OpenAIEmbeddings;
-    llm: ChatOpenAI;
+  embeddings: OpenAIEmbeddings;
+  llm: ChatOpenAI;
 }
 
 export default class WebQA extends StructuredTool {
-    name = 'ask-webpage';
-    description = 'Use this when you need to answer questions about specific webpages';
-    schema = z.object({
-        question: z.string().describe('should be a question on response content'),
-        url: z.string().describe('should be a string'),
-    });
+  name = 'ask-webpage';
+  description = 'Use this when you need to answer questions about specific webpages';
+  schema = z.object({
+    question: z.string().describe('should be a question on response content'),
+    url: z.string().describe('should be a string'),
+  });
 
-    private llm: ChatOpenAI;
-    private embeddings: OpenAIEmbeddings;
+  private llm: ChatOpenAI;
+  private embeddings: OpenAIEmbeddings;
 
-    constructor({ llm, embeddings }: WebQAArgs) {
-        super();
-        this.llm = llm;
-        this.embeddings = embeddings;
+  constructor({ llm, embeddings }: WebQAArgs) {
+    super();
+    this.llm = llm;
+    this.embeddings = embeddings;
+  }
+
+  async _call({ question, url }: WebQASchema): Promise<string> {
+    console.log(`WebQA question: ${question}, url: ${url}, llm: ${this.llm.modelName}`);
+    try {
+      const loader = new PlaywrightWebBaseLoader(url, {
+        gotoOptions: { waitUntil: 'load' },
+        evaluate: async (
+          page: Page,
+          browser: Browser,
+          response: Response | null,
+        ): Promise<string> => {
+          let contentType = 'text/html';
+          if (response != null) {
+            contentType = response.headers()['content-type'];
+          }
+          if (contentType.includes('text/html')) {
+            const frames = page.frames();
+            const contents = await Promise.all(frames.map(evaluateFrameContent));
+            return contents.join('\n\n');
+          }
+          return await response!.text();
+        },
+      });
+
+      const maxToken = getMaxTokens(this.llm.modelName);
+
+      const chunkSize = Math.floor(maxToken * 0.9);
+      const chunkOverlap = Math.floor(chunkSize * 0.2);
+      const lengthFunction = this.llm.getNumTokens;
+
+      const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize,
+        chunkOverlap,
+        lengthFunction,
+      });
+
+      const docs = await loader.loadAndSplit(splitter);
+
+      const combineDocumentsChain = await loadQARefineChain(this.llm);
+
+      // If the number of documents is greater than 4, we use RetrievalQAChain with MemoryVectorStore.
+      // This is more efficient for large documents as it allows us to perform similarity search on the documents in memory.
+
+      if (docs.length <= 4) {
+        console.log(`< ======= docs (total: ${docs.length}) ======= >`);
+        docs.forEach((doc, i) => {
+          console.log(`\n[Document ${i}]\n`);
+          console.log(doc);
+        });
+
+        // If there are 4 or fewer documents, use the original method.
+        const answer = await combineDocumentsChain.call({
+          input_documents: docs,
+          question,
+        });
+        return answer.output_text;
+      } else {
+        // If there are more than 4 documents, use the RetrievalQAChain.
+        const vectorStore = await MemoryVectorStore.fromDocuments(docs, this.embeddings);
+        const retriever = vectorStore.asRetriever();
+
+        const relevantDocs = await retriever.getRelevantDocuments(question);
+        console.log(
+          `< ======= docs (relevant: ${relevantDocs.length}, total: ${docs.length}) ======= >`,
+        );
+        relevantDocs.forEach((doc, i) => {
+          console.log(`\n[Relevant Document ${i}]\n`);
+          console.log(doc);
+        });
+
+        const chain = new RetrievalQAChain({
+          combineDocumentsChain: combineDocumentsChain,
+          retriever: retriever,
+        });
+        const result = await chain.call({ query: question });
+        return result.text;
+      }
+    } catch (e) {
+      console.error(e);
+      throw new Error(String(e));
     }
-
-    async _call({ question, url }: WebQASchema): Promise<string> {
-        console.log(`WebQA question: ${question}, url: ${url}, llm: ${this.llm.modelName}`);
-        try {
-            const loader = new PlaywrightWebBaseLoader(url, {
-                gotoOptions: { waitUntil: 'load' },
-                evaluate: async (page: Page, browser: Browser, response: Response | null): Promise<string> => {
-                    let contentType = 'text/html';
-                    if (response != null) {
-                        contentType = response.headers()['content-type'];
-                    }
-                    if (contentType.includes('text/html')) {
-                        const frames = page.frames();
-                        const contents = await Promise.all(frames.map(evaluateFrameContent));
-                        return contents.join('\n\n');
-                    }
-                    return await response!.text();
-                }
-            });
-
-            const maxToken = getMaxTokens(this.llm.modelName);
-
-            const chunkSize = Math.floor(maxToken * 0.9);
-            const chunkOverlap = Math.floor(chunkSize * 0.2);
-            const lengthFunction = this.llm.getNumTokens;
-
-            const splitter = new RecursiveCharacterTextSplitter({
-                chunkSize,
-                chunkOverlap,
-                lengthFunction,
-            });
-
-            const docs = await loader.loadAndSplit(splitter);
-
-            const combineDocumentsChain = await loadQARefineChain(this.llm);
-
-            // If the number of documents is greater than 4, we use RetrievalQAChain with MemoryVectorStore.
-            // This is more efficient for large documents as it allows us to perform similarity search on the documents in memory.
-
-            if (docs.length <= 4) {
-                console.log(`< ======= docs (total: ${docs.length}) ======= >`);
-                docs.forEach((doc, i) => {
-                    console.log(`\n[Document ${i}]\n`);
-                    console.log(doc);
-                });
-
-                // If there are 4 or fewer documents, use the original method.
-                const answer = await combineDocumentsChain.call({
-                    input_documents: docs,
-                    question,
-                });
-                return answer.output_text;
-            } else {
-                // If there are more than 4 documents, use the RetrievalQAChain.
-                const vectorStore = await MemoryVectorStore.fromDocuments(docs, this.embeddings);
-                const retriever = vectorStore.asRetriever();
-
-                const relevantDocs = await retriever.getRelevantDocuments(question);
-                console.log(`< ======= docs (relevant: ${relevantDocs.length}, total: ${docs.length}) ======= >`);
-                relevantDocs.forEach((doc, i) => {
-                    console.log(`\n[Relevant Document ${i}]\n`);
-                    console.log(doc);
-                });
-
-                const chain = new RetrievalQAChain({
-                    combineDocumentsChain: combineDocumentsChain,
-                    retriever: retriever,
-                });
-                const result = await chain.call({ query: question });
-                return result.text;
-            }
-        } catch (e) {
-            console.error(e);
-            throw new Error(String(e));
-        }
-    }
+  }
 }
 
 module.exports = WebQA;
--- ask.ts
+++ ask.ts
@@ -12,79 +12,79 @@ import * as yargs from 'yargs';
 dotenv.config();
 
 const DEFAULT_CONFIG = {
-    settings: {
-        chat_model: 'gpt-4',
-        temperature: '0',
-    },
+  settings: {
+    chat_model: 'gpt-4',
+    temperature: '0',
+  },
 };
 
 const readFile = util.promisify(fs.readFile);
 
 async function loadConfig(configFile: string) {
-    let config = DEFAULT_CONFIG;
-    if (fs.existsSync(configFile)) {
-        const fileData = await readFile(configFile, 'utf-8');
-        config = JSON.parse(fileData);
-    }
-    return config;
+  let config = DEFAULT_CONFIG;
+  if (fs.existsSync(configFile)) {
+    const fileData = await readFile(configFile, 'utf-8');
+    config = JSON.parse(fileData);
+  }
+  return config;
 }
 
 async function loadTools(config: any) {
-    const llm = new ChatOpenAI({
-        modelName: config.settings.chat_model,
-        temperature: 0,
-        openAIApiKey: process.env.OPENAI_API_KEY,
-    });
-    const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY });
-    let pineconeApiKey = process.env.PINECONE_API_KEY;
-    let pineconeEnvironment = process.env.PINECONE_ENVIRONMENT;
-    let indexName = process.env.PINECONE_INDEX;
-    const client = new PineconeClient();
-    await client.init({
-        apiKey: pineconeApiKey!,
-        environment: pineconeEnvironment!,
-    });
-    const pineconeIndex = client.Index(indexName!);
-    return [
-        new WebQA({ llm, embeddings }),
-        new BookQA({
-            llm,
-            embeddings,
-            pineconeIndex,
-        }),
-    ];
+  const llm = new ChatOpenAI({
+    modelName: config.settings.chat_model,
+    temperature: 0,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+  const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY });
+  let pineconeApiKey = process.env.PINECONE_API_KEY;
+  let pineconeEnvironment = process.env.PINECONE_ENVIRONMENT;
+  let indexName = process.env.PINECONE_INDEX;
+  const client = new PineconeClient();
+  await client.init({
+    apiKey: pineconeApiKey!,
+    environment: pineconeEnvironment!,
+  });
+  const pineconeIndex = client.Index(indexName!);
+  return [
+    new WebQA({ llm, embeddings }),
+    new BookQA({
+      llm,
+      embeddings,
+      pineconeIndex,
+    }),
+  ];
 }
 
 async function initAgentWithTools(config: any) {
-    const chat = new ChatOpenAI({
-        modelName: config.settings.chat_model,
-        temperature: parseFloat(config.settings.temperature),
-        openAIApiKey: process.env.OPENAI_API_KEY,
-    });
-    const tools = await loadTools(config);
-    const agent = initializeAgentExecutorWithOptions(tools, chat, {
-        agentType: 'openai-functions',
-        verbose: true,
-    });
-    return agent;
+  const chat = new ChatOpenAI({
+    modelName: config.settings.chat_model,
+    temperature: parseFloat(config.settings.temperature),
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+  const tools = await loadTools(config);
+  const agent = initializeAgentExecutorWithOptions(tools, chat, {
+    agentType: 'openai-functions',
+    verbose: true,
+  });
+  return agent;
 }
 
 async function processMessagesFromFile(filePath: string, config: any) {
-    const agent = await initAgentWithTools(config);
-    const fileData = await readFile(filePath, 'utf-8');
-    const messages = JSON.parse(fileData);
-    for (const message of messages) {
-        const responseMessage = await agent.run(message);
-        console.log(responseMessage);
-    }
+  const agent = await initAgentWithTools(config);
+  const fileData = await readFile(filePath, 'utf-8');
+  const messages = JSON.parse(fileData);
+  for (const message of messages) {
+    const responseMessage = await agent.run(message);
+    console.log(responseMessage);
+  }
 }
 
 async function main() {
-    const argv = await yargs
-        .option('config_file', { default: 'config.json' })
-        .option('message_file', { demandOption: true }).argv;
-    const config = await loadConfig(argv.config_file);
-    await processMessagesFromFile(argv.message_file as string, config);
+  const argv = await yargs
+    .option('config_file', { default: 'config.json' })
+    .option('message_file', { demandOption: true }).argv;
+  const config = await loadConfig(argv.config_file);
+  await processMessagesFromFile(argv.message_file as string, config);
 }
 
 main();
